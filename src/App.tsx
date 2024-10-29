6cbec4c0f768d6ac857e37d7293d6570a8969e0d
Nidurolak <jaewoon08@gmail.com>
2024-10-29 22:50:27 +0900

    feat : tailwind CSS 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="font-mabinogi testAligin">
+    <div className="font-mabinogi Mid_Aligin Full_WH">
       <h1 className="text-2xl font-bold">Mabinogi Classic Font 적용!</h1>
       <p>이 문장은 글로벌 글꼴로 지정된 MabinogiClassic 글꼴을 사용합니다.</p>
     </div>
