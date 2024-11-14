2b026d9c21748e9dbd1501eeaa42e565bc741380
Nidurolak <jaewoon08@gmail.com>
2024-11-15 01:59:31 +0900

    css : 모달 스타일링

--- src/components/Horomodal.tsx
+++ src/components/Horomodal.tsx
@@ -7,8 +7,19 @@ import { getTest } from '../utils/api/api';
 
 function Horomodal() {
 
-
-    return (<div className="HoroModal_Back">asdasdasdsda</div>)
+    const bidOptions = [
+        { id: 1, label: '전체', value: 'all' },
+        { id: 2, label: '음식', value: 'food' },
+    ];
+    return (<div className="HoroModal_Back">
+        <div className='HoroModal_Header'><h3 className='HoroModal_XMark HoroModal_XMark_OnMouse'>X</h3></div>
+        asdasdasdsda
+        <select>
+            {bidOptions.map((val) => (
+                <option key={val.id} value={val.value}>{val.label}</option>
+            ))}
+        </select>
+    </div>)
 }
 
 export default Horomodal
---
+++ src/utils/types.tsx
@@ -0,0 +1,9 @@
+import { type } from "os";
+import React from "react";
+import { ButtonHTMLAttributes } from "react";
+
+export interface BidOption {
+    id: number;
+    label: string;
+    value: string
+}
