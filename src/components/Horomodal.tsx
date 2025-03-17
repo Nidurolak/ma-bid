72424d73f282165b2ab353321374fb32a7030d1f
Nidurolak <jaewoon08@gmail.com>
2025-03-17 23:48:28 +0900

    잔디심기

--- src/components/Horomodal.tsx
+++ src/components/Horomodal.tsx
@@ -8,8 +8,13 @@ import { getTest } from '../utils/api/api';
 function Horomodal() {
 
     const bidOptions = [
-        { id: 1, label: '전체', value: 'all' },
-        { id: 2, label: '음식', value: 'food' },
+        { id: 0, label: '전체', value: 'all' },
+        { id: 1, label: '음식', value: 'food' },
+        { id: 2, label: '장비', value: 'food' },
+        { id: 3, label: '생활', value: 'food' },
+        { id: 4, label: '기타', value: 'food' },
+        { id: 5, label: '소모품', value: 'food' },
+        { id: 6, label: '생활재료', value: 'food' },
     ];
     return (<div className="HoroModal_Back">
         <div className='HoroModal_Header'><h3 className='HoroModal_XMark HoroModal_XMark_OnMouse'>X</h3></div>
